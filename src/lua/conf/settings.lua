3cbc1833fc29adeb354cd69c88e7e6567b4c469b
Ray <rayydl@gmail.com>
2015-06-15 17:29:05 +0800

    add log module

--- src/lua/conf/settings.lua
+++ src/lua/conf/settings.lua
@@ -1,2 +1,5 @@
-Token = ""
-EncodingAESKey = ""
+Token = "1992knightdfiraydcom0310"
+EncodingAESKey = "RjwliKyFTjRQuRBYukGg6aB2sB5c4tScN9XwkRWjchP"
+
+LOG_PATH = "/yundisk/log/wechat/wechat.log"
+LOG_LEVEL = "DEBUG"
--- src/lua/ext/varify_server.lua
+++ src/lua/ext/varify_server.lua
@@ -1,5 +1,6 @@
 local Class = require 'util.Class'
-local log = require 'util.log'
+local logger = require 'util.log'
+local cjson = require 'cjson.safe'
 local ngx = package.loaded['ngx']
 require 'conf.settings'
 
@@ -13,11 +14,13 @@ function WeChatTest:check_server(ngx)
     local timestamp = args.timestamp
     local nonce = args.nonce
     local echostr = args.echostr
+    logger:debug('wechat uri args: %s', cjson.encode(args))
 
     if signature and timestamp and nonce and echostr then
         local t = {token, timestamp, nonce}
         table.sort(t)
         local s = ngx.sha1_bin(table.concat(t))
+        logger:debug('calculated signature: %s', tostring(s))
         if s == signature then
             ngx.say(echostr)
             ngx.exit(200)
